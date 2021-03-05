671e36a8760eed300debce044e86d1508cc0572f
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-05 13:05:27 -0300

    Makes course class "Função print e variáveis"

---
+++ src/aulas/01.py
@@ -0,0 +1,17 @@
+# Função print e variáveis
+print("olá mundo")
+
+print("Brasil ganhou 5 mundiais")
+
+print("Brasil","ganhou","5","mundiais", sep=" ")
+
+print("Brasil","ganhou","5","mundiais", sep="+")
+
+print("Brasil","ganhou","5","mundiais", sep="\n")
+
+print("Brasil","ganhou","5","mundiais", end="\n")
+
+
+pais = "Itália"
+quantidade = 4
+print(pais, "ganhou", quantidade, "títulos mundiais")
