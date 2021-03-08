3656eb0b8db75298e34fafb1684ddb7c4267ac67
Claudi Martins de Freitas <claudi.freitas.prs@synergiaconsultoria.com.br>
2021-03-08 16:02:14 -0300

    Makes course class "Escolhendo uma palavra"

--- src/jogos/forca.py
+++ src/jogos/forca.py
@@ -1,11 +1,26 @@
 
+import random
+
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
+    arquivo = open("jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
     letras_acertadas = ["_" for letra in palavra_secreta]
+
+    print(palavra_secreta)
 
     enforcou = False
     acertou = False
